89c203d7ab1ddfec57cf921fb6893d51556cb515
William Hallström <willewilliamhallstrom@gmail.com>
2026-04-27 14:25:02 +0200

    jag bytte ut twitter bootstrap med vanliga bootstrap

--- includes/mainmenu.php
+++ includes/mainmenu.php
@@ -5,6 +5,8 @@ if(!isset($_SESSION["user"])){
     header("Location: login.php");
     echo "yo";
 }
+
+include "Botstrap.php"
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -12,14 +14,8 @@ if(!isset($_SESSION["user"])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=90shjciodpsqI UWI, initial-scale=1.0">
     <title>Document</title>
-    <link ref="stylesheet" href="../bootstrap/css/bootstrap-responsive.css">
-    <link ref="stylesheet" href="../bootstrap/css/bootstrap.css">
-    <script src="../bootstrap/js/bootstrap.js"></script>
-    <script src="../bootstrap/js/bootstrap.min.js"></script>
 </head>
 <body>
-
-<div class="></div>
     
 </body>
 </html>
--- includes/nav.php
+++ includes/nav.php
@@ -1,5 +1,21 @@
-<nav class="site-nav">
-    <ul>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link ref="stylesheet" href="../bootstrap/css/bootstrap-responsive.css">
+    <link ref="stylesheet" href="../bootstrap/css/bootstrap.css">
+    <script src="../bootstrap/js/bootstrap.js"></script>
+    <script src="../bootstrap/js/bootstrap.min.js"></script>
+</head>
+</html>
+<body>
+    <nav class="site-nav">
+    <div class="row">
+    <div class="col"></div>
+     <div class="col">
+        <ul>
         <li><a href="home.php">Home</a></li>
         <li><a href="search.php">Search</a></li>
         <li><a href="message.php">Message</a></li>
@@ -7,4 +23,8 @@
         <li><a href="profile.php">Profile</a></li>
         <li><a href="settings.php">Settings</a></li>
     </ul>
+     </div>
+     <div class="col"></div>
+</div>
 </nav>
+</body>
